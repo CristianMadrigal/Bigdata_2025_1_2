e786989adb52cb2bd5444b70dd9f6d8df4a88a12
CristianMadrigal <cristian.madrigal@est.iudigital.edu.co>
2025-05-10 18:43:20 +0000

    creacion setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup    
+
+setup(
+    name="edu_bigdata",
+    author="Cristian Madrigal",
+    author_email="cristian.madrigal@est.iudigital.edu.co",
+    description="",
+    install_requires=[ 
+        "pandas",
+        "opencsv",
+        "request",
+        "BeautifulSoup4"
+    ]
+)
